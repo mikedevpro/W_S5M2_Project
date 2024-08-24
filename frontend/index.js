fd9485723ac7fb5966aa8a3dc14043bb47d982f8
Michael Nobles <Mnobles33@gmail.com>
2024-08-24 08:56:20 -0700

    Last Commit

--- frontend/index.js
+++ frontend/index.js
@@ -74,6 +74,7 @@ function moduleProject2() {
     let isDown = evt.key === keys.down
     let isLeft = evt.key === keys.left
     let isRight = evt.key === keys.right
+    let isSpacebar = evt.key === keys.space
 
     let targeted = document.querySelector('.targeted')
 
@@ -101,7 +102,29 @@ function moduleProject2() {
       }
     }
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
+    else if (isSpacebar) {
+      let mosquito = targeted.firstChild
+      
+      if (mosquito && mosquito.dataset.status === 'alive' ) {
+        mosquito.dataset.status = 'dead'
+        mosquito.parentElement.style.backgroundColor = 'red'
+      }
 
+      let liveMosquitoes = document.querySelectorAll('[data-status=alive]')
+      if (!liveMosquitoes.length) {
+        let elapsed = getTimeElapsed()
+        document.querySelector('p.info').textContent = 
+        `Extermination completed in ${elapsed / 1000} seconds!`
+
+        let restartBtn = document.createElement('button')
+        restartBtn.textContent = 'Restart'
+        restartBtn.addEventListener('click', () => {
+          location.reload()
+        })
+        document.querySelector('h2').insertAdjacentElement('beforeend', restartBtn)
+        
+      }
+    }
     // 👉 TASK 5 - End the game 👈
     
   })
